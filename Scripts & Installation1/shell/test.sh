67c54a26860ea283b50e6c4b3c18c81082112040
trueredfence <redfenceofficial@gmail.com>
2024-11-05 20:25:22 +0530

    shell updated

--- Scripts & Installation1/shell/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set pass Hj78%^\$dfh#@@123njdyHG
-spawn ssh root@81.17.28.123 ls -lth /var/wxw/sundays | grep 'Oct 30' | awk '{print \$NF}'
-expect "*?assword*"
-send -- "$pass\r"
-interact
